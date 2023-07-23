1f7e9813578e7d3081f1a94125ce17af36f3b3c0
nazar-si <nazari61@yandex.ru>
2023-07-23 17:31:29 +0300

    chore: added another function to get all params

--- src/domain/query.ts
+++ src/domain/query.ts
@@ -1,11 +1,17 @@
-import { GetRandomRequestParams} from "./types";
+import { GetRandomRequestParams, GetRequestParams } from "./types";
 
 const BaseURL = " https://quote-garden.onrender.com/api/v3/quotes";
 
-export const getQuotesQuery = async (params?: GetRandomRequestParams) => {
-    if (!params) return `${BaseURL}/random?`;
-    const author = params.author ? `&author=${params.author}` : "";
-    const genre = params.genre ? `&genre=${params.genre}` : "";
-    const count = params.count ? `&count=${params.count}` : "";
-    return `${BaseURL}/random?${author}${genre}${count}`;
+const stringifyParams = (params : any) => {
+    return Object.fromEntries(Object.entries(params).map(([key, value] : [string, any]) => [key, value.toString() as string]))
+}
+
+export const getRandomQuotesQuery = async (params?: GetRandomRequestParams) => {
+    if (!params) return `${BaseURL}/random`
+    return `${BaseURL}/random?${new URLSearchParams(stringifyParams(params))}}`
+};
+
+export const getQuotesQuery = async (params?: GetRequestParams) => {
+    if (!params) return `${BaseURL}`
+    return `${BaseURL}?${new URLSearchParams(stringifyParams(params))}}`
 };
